14f0a69f33f79619ec066399fd1256639ddd6274
Em1li4 <148295178+Em1li4@users.noreply.github.com>
2023-11-01 23:07:51 -0700

    Update Project user interaction.py

--- BLUEFRUIT/Project user interaction/Project user interaction.py
+++ BLUEFRUIT/Project user interaction/Project user interaction.py
@@ -1,3 +1,8 @@
+//This is the code that I used to make my user interaction. The language is Python and I used MU as a code editor.
+//It allows the user to interact with the program, by allowing the user to select what color wants to be displayed and in which neopixel.
+//The values for the color run in a RGB scale.
+//It allows the user to light up different neopixels and different colors each time.
+
 from adafruit_circuitplayground import cp
 while True:
     pix_num = int(input("which neopixel turn on? "))
